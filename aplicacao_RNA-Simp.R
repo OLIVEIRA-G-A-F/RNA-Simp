2f7cfbb8be5fbf69047c7a3898f969e69ef071fa
OLIVEIRA-G-A-F <oliveira.guilhermeaf@gmail.com>
2024-11-27 18:16:14 -0300

    Update aplicacao_RNA-Simp.R

--- aplicacao_RNA-Simp.R
+++ aplicacao_RNA-Simp.R
@@ -7,6 +7,6 @@ library(tidyverse)
 
 #Utilizando o modelo
 modelo = readRDS("soil_moisture_model.rda") #carrega o modelo
-dados = read.csv("sua_planilha.csv") #carrega os dados
+dados = read.csv("exemplo_dados.csv") #carrega os dados
 dados= dados %>% janitor::clean_names() #padroniza o cabeçalho
 preditos = predict(modelo, new_data= dados) #aplicação do modelo para estimativa da umidade do solo
